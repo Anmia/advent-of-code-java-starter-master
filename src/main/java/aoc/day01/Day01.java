18d5a0f04961cd7780eb72eb4d57e0f2a53b7b84
Anmia <nerdikus_vulgeris@hotmail.com>
2020-12-03 19:23:57 +0100

    day1done

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -8,12 +8,58 @@ public class Day01 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+
+        int listLength = input.size();
+        int x;
+        int y;
+        int z;
+        int result = 0;
+        for (int i = 0; i < listLength; i++) {
+            x = Integer.parseInt(input.get(i));
+            for (int j = 0; j < listLength; j++) {
+                y = Integer.parseInt(input.get(j));
+                z = x + y;
+
+                if ((z == 2020) && (x != y)) {
+                    result = x * y;
+                }
+            }
+        }
+
+        String boom = "" + result + "";
+
+        return boom;
+        //return input.isEmpty() ? "" : input.get(0);
     }
 
     @Override
     public String part2(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        int listLength = input.size();
+        int a;
+        int b;
+        int c;
+        int d;
+        int result = 0;
+        for (int i = 0; i < listLength; i++) {
+            a = Integer.parseInt(input.get(i));
+            for (int j = 0; j < listLength; j++) {
+                b = Integer.parseInt(input.get(j));
+                for (int k = 0; k < listLength; k++) {
+                    c = Integer.parseInt(input.get(k));
+                    d = a + b + c;
+
+                    if (d == 2020) {
+                        result = a * b * c;
+                    }
+                }
+
+            }
+        }
+
+        String boom = "" + result + "";
+
+        return boom;
+
     }
 
 }
